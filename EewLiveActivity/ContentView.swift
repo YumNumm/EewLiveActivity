510a13d098fadee6431d2e8b8d5ba3653c4615a9
YumNumm <onory0917@gmail.com>
2024-12-12 23:53:38 +0900

    add initial application

---
+++ EewLiveActivity/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EewLiveActivity
+//
+//  Created by 尾上 遼太朗 on 2024/12/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EewLiveActivity/EewLiveActivityApp.swift
@@ -0,0 +1,17 @@
+//
+//  EewLiveActivityApp.swift
+//  EewLiveActivity
+//
+//  Created by 尾上 遼太朗 on 2024/12/12.
+//
+
+import SwiftUI
+
+@main
+struct EewLiveActivityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
