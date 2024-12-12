bdd8d6adf0221747a844640d922eb3fe8cd22cd9
YumNumm <onory0917@gmail.com>
2024-12-12 23:58:00 +0900

    add: LiveActivityExtension

---
+++ LiveActivity/LiveActivityBundle.swift
@@ -0,0 +1,17 @@
+//
+//  LiveActivityBundle.swift
+//  LiveActivity
+//
+//  Created by 尾上 遼太朗 on 2024/12/12.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct LiveActivityBundle: WidgetBundle {
+    var body: some Widget {
+        LiveActivity()
+        LiveActivityLiveActivity()
+    }
+}
